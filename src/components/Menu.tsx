e88f5a6d4d9122bb882f2c1930e9805f6b9c32eb
Ankit Sharma <ankitsharma2615@gmail.com>
2025-03-31 17:30:54 +0530

    feat: add class schedule calender & complete homepage

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,5 @@
 import menuItems from "@/constants/menuItems";
+import { role } from "@/lib/data";
 import Link from "next/link";
 
 const Menu = () => {
@@ -9,16 +10,20 @@ const Menu = () => {
           <span className="hidden lg:block text-muted-foreground font-normal my-4">
             {item.title}
           </span>
-          {item.items.map((subItem) => (
-            <Link
-              href={subItem.href}
-              key={subItem.label}
-              className="flex items-center justify-center lg:justify-start gap-4 text-muted-foreground py-2"
-            >
-              <subItem.icon className="w-5 h-5 inline-block mr-2" />
-              <span className="hidden lg:block">{subItem.label}</span>
-            </Link>
-          ))}
+          {item.items.map((subItem) => {
+            if (subItem.visible.includes(role)) {
+              return (
+                <Link
+                  href={subItem.href}
+                  key={subItem.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-muted-foreground py-2 md:px-2 rounded-md hover:bg-SkyLight"
+                >
+                  <subItem.icon className="w-5 h-5 inline-block mr-2" />
+                  <span className="hidden lg:block">{subItem.label}</span>
+                </Link>
+              );
+            }
+          })}
         </div>
       ))}
     </div>
