1921e199e2ca899c687d382daa25851b8971ef2b
Ankit Sharma <ankitsharma2615@gmail.com>
2025-03-31 23:20:50 +0530

    feat: add parents&students table

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -1,21 +1,17 @@
-import Pagination from "@/components/Pagination";
-import Table from "@/components/Table";
-import TableSearch from "@/components/TableSearch";
-import { Button } from "@/components/ui/button";
-import { role, teachersData } from "@/lib/data";
-import { Eye, Plus, Settings2, SortDesc, Trash2 } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
+import Table from "@/components/Table";
+import { parentsData, role } from "@/lib/data";
+import { Button } from "@/components/ui/button";
+import Pagination from "@/components/Pagination";
+import TableSearch from "@/components/TableSearch";
+import { Eye, Plus, Settings2, SortDesc, Trash2 } from "lucide-react";
 
-type Teacher = {
+type Parent = {
   id: number;
-  teacherId: string;
   name: string;
+  students: string[];
   email?: string;
-  photo: string;
   phone: string;
-  subjects: string[];
-  classes: string[];
   address: string;
 };
 
@@ -25,19 +21,9 @@ const columns = [
     accessor: "info",
   },
   {
-    header: "Teacher ID",
-    accessor: "teacherId",
+    header: "Student Names",
+    accessor: "students",
     className: "hidden md:table-cell",
-  },
-  {
-    header: "Subjects",
-    accessor: "subjects",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Classes",
-    accessor: "classes",
-    className: "hidden lg:table-cell",
   },
   {
     header: "Phone",
@@ -55,29 +41,20 @@ const columns = [
   },
 ];
 
-const TeacherListPage = () => {
-  const renderRow = (item: Teacher) => (
+const ParentListPage = () => {
+  const renderRow = (item: Parent) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-PurpleLight"
     >
       <td className="flex items-center gap-4 p-4">
-        <Image
-          src={item.photo}
-          alt="teacher_image"
-          width={40}
-          height={40}
-          className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
-        />
         <div className="flex flex-col">
           <h3 className="font-semibold">{item.name}</h3>
           <p className="text-xs text-gray-500">{item?.email}</p>
         </div>
       </td>
 
-      <td className="hidden md:table-cell">{item.teacherId}</td>
-      <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
-      <td className="hidden lg:table-cell">{item.classes.join(",")}</td>
+      <td className="hidden md:table-cell">{item.students.join(",")}</td>
       <td className="hidden lg:table-cell">{item.phone}</td>
       <td className="hidden lg:table-cell">{item.address}</td>
       <td>
@@ -101,7 +78,7 @@ const TeacherListPage = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Parents</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -129,11 +106,11 @@ const TeacherListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={teachersData} />
+      <Table columns={columns} renderRow={renderRow} data={parentsData} />
       {/* PAGINATION */}
       <Pagination />
     </div>
   );
 };
 
-export default TeacherListPage;
+export default ParentListPage;
